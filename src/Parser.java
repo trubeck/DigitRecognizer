915bcaf53580c043f60f5edb7ccd5c71f5d604a0
Gerson Berger <gerson.berger@stud.uni-due.de>
2017-11-20 15:42:14 +0100

    parser progress

--- src/Parser.java
+++ src/Parser.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Parser {
 
-    public int[][][] parseImage(File f){
+    int[][][] parseImage(File f) {
         try {
             FileInputStream images = new FileInputStream(f);
 
@@ -13,18 +13,44 @@ public class Parser {
             int numberOfColumns = (images.read() << 24) | (images.read() << 16) | (images.read() << 8) | (images.read());
             System.out.println(magicNumberImages + ", "+ numberOfImages + ", "+ numberOfRows +", "+ numberOfColumns);
 
-
-
-
+            int[][][] numberSet = new int[numberOfImages][numberOfRows][numberOfColumns];
+    
+            for (int numImage = 0; numImage < numberOfImages; numImage++) {
+                for (int numCol = 0; numCol < numberOfColumns; numCol++) {
+                    for (int numRow = 0; numRow < numberOfRows; numRow++) {
+                        numberSet[numImage][numRow][numCol] = images.read();
+                        if (numberSet[numImage][numRow][numCol] <= 0xF) {
+                            System.out.print("0" + Integer.toHexString(numberSet[numImage][numRow][numCol]));
+                        }
+                        else {
+                            System.out.print(Integer.toHexString(numberSet[numImage][numRow][numCol]));
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+            return numberSet;
+    
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         return null;
     }
 
-    public int[] parseLabel(File f){
-
+    public int[] parseLabel(File f) {
+        try {
+            FileInputStream labels = new FileInputStream(f);
+    
+            int magicNumberLabels = (labels.read() << 24) | (labels.read() << 16) | (labels.read() << 8) | (labels.read());
+            int numberOfLabels = (labels.read() << 24) | (labels.read() << 16) | (labels.read() << 8) | (labels.read());
+            
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
         return null;
     }
 }
